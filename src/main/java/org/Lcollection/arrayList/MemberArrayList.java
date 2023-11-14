bfcb2ee4d98593003cb4aef61819f277d83cfcb7
이혁기 <gurrl9823@gmail.com>
2023-11-14 21:58:35 +0900

    Stack, Queue 구현하기, iterator로 loop 구현하기

--- src/main/java/org/Lcollection/arrayList/MemberArrayList.java
+++ src/main/java/org/Lcollection/arrayList/MemberArrayList.java
@@ -2,6 +2,7 @@ package org.Lcollection.arrayList;
 
 import org.Lcollection.Member;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class MemberArrayList {
 
@@ -16,12 +17,29 @@ public class MemberArrayList {
     }
 
     public boolean rmMember(int id) {
-        for (int i = 0; i < this.arrayList.size(); i++) {
-            if (arrayList.get(i).getId() == id) {
-                this.arrayList.remove(i);
+//        for (int i = 0; i < this.arrayList.size(); i++) {
+//            if (arrayList.get(i).getId() == id) {
+//                this.arrayList.remove(i);
+//                return true;
+//            }
+//        }
+
+        // 위에 for로 구현한 것과 아래의 iterator로 구현한 것은 같다.
+
+        // ArrayList는 Collection interface를 구현한 클래스이기 때문에
+        // Collection 이 가지고 있는 iterator를 호출할 수 있다.
+        // generic 타입으로 쓸 수 있다. 하나씩 볼 객체인 Member로 작성한다.
+        Iterator<Member> iterator = this.arrayList.iterator();
+        while (iterator.hasNext()) {
+            Member member = iterator.next(); // 얘는 원래 Object 타입을 리턴하는데
+                                             // generic 타입으로 Member를 지정했기 때문에 Member를 리턴한다.
+            if (member.getId() == id) {
+                this.arrayList.remove(member);
                 return true;
             }
         }
+
+        System.out.println(id + "이(가) 존재하지 않습니다.");
         return false;
     }
 
@@ -30,6 +48,7 @@ public class MemberArrayList {
             System.out.println(member);
         }
         System.out.println(this.arrayList);
+        System.out.println();
     }
 
 }
---
+++ src/main/java/org/Lcollection/arrayList/QueueTest.java
@@ -0,0 +1,39 @@
+package org.Lcollection.arrayList;
+
+
+import java.util.ArrayList;
+
+class MyQueue {
+    private ArrayList<String> arrayQueue = new ArrayList<>();
+
+    public void enQueue(String data) {
+        this.arrayQueue.add(data);
+    }
+
+    public String deQueue() {
+        int len = this.arrayQueue.size();
+        if (len > 0) {
+            return this.arrayQueue.remove(0);
+        }
+        System.out.println("큐가 비었습니다.");
+        return null;
+    }
+}
+
+public class QueueTest {
+
+    public static void main(String[] args) {
+
+        MyQueue myQueue = new MyQueue();
+
+        myQueue.enQueue("a");
+        myQueue.enQueue("b");
+        myQueue.enQueue("c");
+
+        System.out.println(myQueue.deQueue());
+        System.out.println(myQueue.deQueue());
+        System.out.println(myQueue.deQueue());
+        System.out.println(myQueue.deQueue());
+    }
+
+}
